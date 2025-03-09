857627e9ffe069e4626a2ed8abe0fc4f95277a5e
Sakred_ <egorn0510@gmail.com>
2025-03-09 19:14:57 +0500

    Update 1.1.2 for 1.21.2

--- src/main/java/dev/sakred/elytratweaks/config/ElytraTweaksConfig.java
+++ src/main/java/dev/sakred/elytratweaks/config/ElytraTweaksConfig.java
@@ -5,4 +5,11 @@ public class ElytraTweaksConfig {
     public boolean enableElytraSwap = true;
     public boolean enableLowDurabilityWarning = true;
     public boolean enableElytraSwapOnMace = true;
+    public boolean enableAutoElytraReplace = true;
+    // Customization
+    public boolean enableWarningCustomization = false;
+    public String customDurabilityWarningMessage = "";
+    public int warnDurability1 = 20;
+    public int warnDurability2 = 10;
+    public int warnDurability3 = 5;
 }
