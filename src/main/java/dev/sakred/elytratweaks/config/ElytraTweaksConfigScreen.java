2b7b98a891fb5331afca2badcb7909031aa36ba7
Sakred_ <egorn0510@gmail.com>
2025-03-09 21:10:17 +0500

    Update 1.1.2 for Fabric 1.20

--- src/main/java/dev/sakred/elytratweaks/config/ElytraTweaksConfigScreen.java
+++ src/main/java/dev/sakred/elytratweaks/config/ElytraTweaksConfigScreen.java
@@ -1,6 +1,8 @@
 package dev.sakred.elytratweaks.config;
 
 import dev.isxander.yacl3.api.*;
+import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
+import dev.isxander.yacl3.api.controller.StringControllerBuilder;
 import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -8,7 +10,7 @@ import net.minecraft.text.Text;
 public class ElytraTweaksConfigScreen {
     public static Screen create(Screen parent) {
         return YetAnotherConfigLib.createBuilder()
-                .title(Text.of("Elytra Tweaks v1.1.0+1.21"))
+                .title(Text.of("Elytra Tweaks v1.1.2"))
                 .category(ConfigCategory.createBuilder()
                         .name(Text.of("General"))
                         .option(Option.<Boolean>createBuilder()
@@ -36,6 +38,54 @@ public class ElytraTweaksConfigScreen {
                                         .description(OptionDescription.of(Text.of("Automatically replaces elytra with chestplate when holding a mace and disables swap.")))
                                         .binding(true, () -> ElytraTweaksConfigManager.config.enableElytraSwapOnMace, newValue -> ElytraTweaksConfigManager.config.enableElytraSwapOnMace = newValue)
                                         .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .option(Option.<Boolean>createBuilder()
+                                        .name(Text.of("Auto Elytra Replaces"))
+                                        .description(OptionDescription.of(Text.of("Automatically replaces elytra with other elytra when they are broken")))
+                                        .binding(true, () -> ElytraTweaksConfigManager.config.enableAutoElytraReplace, newValue -> ElytraTweaksConfigManager.config.enableAutoElytraReplace = newValue)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .build())
+                        .build())
+                .category(ConfigCategory.createBuilder()
+                        .name(Text.of("Customization"))
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.of("Warning Customization"))
+                                .option(Option.<Boolean>createBuilder()
+                                        .name(Text.of("Enable Warning Customization"))
+                                        .description(OptionDescription.of(Text.of("Allows customization of low durability warnings.")))
+                                        .binding(false, () -> ElytraTweaksConfigManager.config.enableWarningCustomization, newValue -> ElytraTweaksConfigManager.config.enableWarningCustomization = newValue)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .option(Option.<String>createBuilder()
+                                        .name(Text.of("Custom Warning Message"))
+                                        .description(OptionDescription.of(Text.of("Custom message to display when elytra durability is low.")))
+                                        .binding("", () -> ElytraTweaksConfigManager.config.customDurabilityWarningMessage, newValue -> ElytraTweaksConfigManager.config.customDurabilityWarningMessage = newValue)
+                                        .controller(StringControllerBuilder::create)
+                                        .build())
+                                .option(Option.<Integer>createBuilder()
+                                        .name(Text.of("First Warning Durability"))
+                                        .description(OptionDescription.of(Text.of("Setting the value when the first warning is displayed")))
+                                        .binding(20, () -> ElytraTweaksConfigManager.config.warnDurability1, newValue -> ElytraTweaksConfigManager.config.warnDurability1 = newValue)
+                                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                                .range(1, 100) // Устанавливаем диапазон от 1 до 100
+                                                .step(1)) // Шаг изменения значения
+                                        .build())
+                                .option(Option.<Integer>createBuilder()
+                                        .name(Text.of("Second Warning Durability"))
+                                        .description(OptionDescription.of(Text.of("Setting the value when the second warning is displayed")))
+                                        .binding(10, () -> ElytraTweaksConfigManager.config.warnDurability2, newValue -> ElytraTweaksConfigManager.config.warnDurability2 = newValue)
+                                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                                .range(1, 100) // Устанавливаем диапазон от 1 до 100
+                                                .step(1)) // Шаг изменения значения
+                                        .build())
+                                .option(Option.<Integer>createBuilder()
+                                        .name(Text.of("Third Warning Durability"))
+                                        .description(OptionDescription.of(Text.of("Setting the value when the third warning is displayed")))
+                                        .binding(5, () -> ElytraTweaksConfigManager.config.warnDurability3, newValue -> ElytraTweaksConfigManager.config.warnDurability3 = newValue)
+                                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                                .range(1, 100) // Устанавливаем диапазон от 1 до 100
+                                                .step(1)) // Шаг изменения значения
                                         .build())
                                 .build())
                         .build())
